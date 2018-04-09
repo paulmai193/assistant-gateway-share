27460066849ceabd645fb7803a2e335321933546
Dai Mai <dai.mai@mediastep.com>
2018-04-09 14:35:58 +0700

    add javadoc

--- src/main/java/logia/assistant/share/gateway/AssistantGatewayShareApplication.java
+++ src/main/java/logia/assistant/share/gateway/AssistantGatewayShareApplication.java
@@ -5,11 +5,21 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+/**
+ * The Class AssistantGatewayShareApplication.
+ *
+ * @author Dai Mai
+ */
 @Configuration
 @ComponentScan(basePackages = {"logia.assistant.share.gateway"})
 @PropertySource("classpath:application.properties")
 public class AssistantGatewayShareApplication {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(AssistantGatewayShareApplication.class, args);
 	}
--- src/main/java/logia/assistant/share/gateway/securiry/jwt/AuthoritiesConstants.java
+++ src/main/java/logia/assistant/share/gateway/securiry/jwt/AuthoritiesConstants.java
@@ -1,15 +1,28 @@
 package logia.assistant.share.gateway.securiry.jwt;
 
 
+/**
+ * The Class AuthoritiesConstants.
+ *
+ * @author Dai Mai
+ */
 public final class AuthoritiesConstants {
+    
+    /** The Constant ADMIN. */
     public static final String ADMIN = "ROLE_ADMIN";
 
+    /** The Constant USER. */
     public static final String USER = "ROLE_USER";
 
+    /** The Constant ANONYMOUS. */
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
     
+    /** The Constant SYSTEM. */
     public static final String SYSTEM = "ROLE_SYSTEM";
     
+    /**
+     * Instantiates a new authorities constants.
+     */
     private AuthoritiesConstants() {
     }
 }
